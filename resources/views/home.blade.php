e6a503e2b4f68b3c3082a1a61f0969d6950b6f00
Makibo93 <makibo93@users.noreply.github.com>
2021-02-21 17:16:35 +0700

    ProfileCont
    Continue to profile progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-        
+        <div class="col-3 p-5">
+            <img src="/img/aveng.png" style="height: 130px;" class="rounded-circle">
         </div>
-        
+        <div class="col-9 pt-5">
+            <div><h1>GroodTheCool</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>1 </strong>post</div>
+                <div class="pr-5"><strong>1000k </strong>follower</div>
+                <div class="pr-5"><strong>0 </strong>following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Guardian Of Galaxy</div>
+            <div>We're protect the galaxy & EARTH</div>
+            <div>www.marvel.com</div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="/img/aveng.png" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="/img/aveng.png" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="/img/aveng.png" class="w-100">
+            </div>
+        </div>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ProfilesController@index')->name('home');
